3809f3d7611399932d34e3dbcf250bda741941da
keysama <834529965@qq.com>
2019-08-02 14:15:30 +0800

    '删除多余的判断'

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,9 @@
 import axios from 'axios';
-import {api_url,AxiosWithCredentials} from "@/config";
+import {api_base_url,AxiosWithCredentials} from "@/config";
 import { message } from 'antd';
 
-axios.defaults.baseURL = api_url;
+axios.defaults.baseURL = api_base_url;
 axios.defaults.withCredentials = AxiosWithCredentials;
-
-axios.interceptors.request.use(function (config) {    // 这里的config包含每次请求的内容
-    let token = localStorage.getItem('token');
-    if (token) {
-          config.headers.Authorization = 'Bearer '+ `${token}`;
-    }
-    return config;
-}, function (err) {
-    console.log('err',err)
-})
 
 axios.interceptors.request.use(function (config) {    // 这里的config包含每次请求的内容
     let token = localStorage.getItem('token');
